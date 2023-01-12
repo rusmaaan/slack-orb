db893fa38cfaad0f9f72a7ba260a115b8aa77c09
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-01-12 11:30:09 -0800

    add set

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -3,4 +3,5 @@
 # Workaround for Windows Support
 # For details, see: https://github.com/CircleCI-Public/slack-orb/pull/380
 # shellcheck source=/dev/null
+set -x
 eval printf '%s' "$SLACK_SCRIPT_NOTIFY"
